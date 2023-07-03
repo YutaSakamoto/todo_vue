5ced94a2900246bf14e7e395e62e232cd89802a9
YutaSakamoto <yutasakamoto@YSs-MacBook-Air.local>
2023-07-03 13:19:37 +0900

    ローカルストレージ追加と表示

--- main.js
+++ main.js
@@ -1,6 +1,35 @@
 new Vue({
     el: '#app',
     data: {
-        today: new Date().toLocaleDateString()
+        today: new Date().toLocaleDateString(),
+        newTask: {
+            id: '',
+            name: '',
+            discription: '',
+            status: '',
+            completDate: '',
+        },
+        tasks: []
+    },
+    created() {
+        const tasks = localStorage.getItem('tasks');
+        if (tasks) {
+            this.tasks = JSON.parse(tasks);
+        }
+    },
+    methods: {
+        addTask() {
+            if (this.newTask.id && this.newTask.name && this.newTask.description && this.newTask.status) {
+                this.tasks.push({ ...this.newTask });
+                this.newTask = {
+                    id: '',
+                    name: '',
+                    description: '',
+                    status: '',
+                    completionDate: ''
+                };
+                localStorage.setItem('tasks', JSON.stringify(this.tasks));
+            }
+        }
     }
 });
